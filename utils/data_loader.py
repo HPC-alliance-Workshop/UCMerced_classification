d09716a13f8977fbf835748479beb5f4797a8750
lamiaeel <lamiae.elmendili@gmail.com>
2023-08-20 10:13:36 -0400

    dataloader

---
+++ utils/data_loader.py
@@ -0,0 +1,30 @@
+import os
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__" :
+
+    print("start")
+
+
